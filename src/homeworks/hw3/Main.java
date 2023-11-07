e2e93cce8bbe0a1687a6fd80b2bc4c14f6c261ff
Taya Inakova <punkgrrl@yandex.ru>
2023-11-07 19:54:27 +0300

    homework 3 done

--- src/homeworks/hw3/Main.java
+++ src/homeworks/hw3/Main.java
@@ -5,14 +5,7 @@ import homeworks.hw3.data.WriteToFile;
 
 public class Main {
     public static void main(String[] args) {
-        Data data1 = new Data("Перов", "Пётр", "Петрович",
-                "12.12.2000", "89536005555", "m");
-        Data data2 = new Data("Иванов", "Иван", "Иваноич",
-                "12.12.2001", "89536005555", "m");
-        Data data3 = new Data("Иванов", "Сергей", "Иваноич",
-                "12.12.2002", "89536005555", "m");
-        WriteToFile.writeToFile(data1);
-        WriteToFile.writeToFile(data2);
-        WriteToFile.writeToFile(data3);
+        Data data = new Data();
+        WriteToFile.writeToFile(data);
     }
 }
